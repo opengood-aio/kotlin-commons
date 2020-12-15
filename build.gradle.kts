c508f2f1002170fa60baa88b7958230918569638
Chris Jaehnen <cjaehnen@me.com>
2020-12-15 14:53:06 -0500

    Test beta version with release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import io.opengood.gradle.enumeration.ProjectType
 
 plugins {
-    id("io.opengood.gradle.config") version "1.3.1"
+    id("io.opengood.gradle.config") version "1.4.3-SNAPSHOT"
 }
 
 group = "io.opengood.commons"
