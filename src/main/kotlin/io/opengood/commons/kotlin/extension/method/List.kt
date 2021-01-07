244648425bca56ecac533c44e7f4529a9c5ba8e7
Chris Jaehnen <cjaehnen@me.com>
2021-01-07 09:18:01 -0500

    Add List containsIgnoreCase extension method

---
+++ src/main/kotlin/io/opengood/commons/kotlin/extension/method/List.kt
@@ -0,0 +1,4 @@
+package io.opengood.commons.kotlin.extension.method
+
+fun List<String>.containsIgnoreCase(element: String) =
+    this.any { it.contains(element, ignoreCase = true) }
---
+++ src/test/kotlin/io/opengood/commons/kotlin/extension/method/ListTest.kt
@@ -0,0 +1,25 @@
+package io.opengood.commons.kotlin.extension.method
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.booleans.shouldBeTrue
+
+class ListTest : FunSpec({
+
+    test("list containsIgnoreCase extension method returns true when case insensitive string contained in list") {
+        val list = listOf<String>("foo", "bar")
+
+        list.containsIgnoreCase("foo").shouldBeTrue()
+    }
+
+    test("list containsIgnoreCase extension method returns true when case sensitive string contained in list") {
+        val list = listOf<String>("foo", "bar")
+
+        list.containsIgnoreCase("Foo").shouldBeTrue()
+    }
+
+    test("list containsIgnoreCase extension method returns false when string not contained in list") {
+        val list = listOf<String>("foo", "bar")
+
+        list.containsIgnoreCase("Foo").shouldBeTrue()
+    }
+})
